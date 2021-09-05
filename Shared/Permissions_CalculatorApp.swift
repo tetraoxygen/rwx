b6cd2066ad3cef7696c81d7ea6dfd3d2658dbb5d
Charlie Welsh <charlie@allotrope.xyz>
2021-09-05 15:58:22 -0700

    Initial Commit

---
+++ Shared/Permissions_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Permissions_CalculatorApp.swift
+//  Shared
+//
+//  Created by Charlie Welsh on 9/5/21.
+//
+
+import SwiftUI
+
+@main
+struct Permissions_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
